7412150ba34c711bd63c706a326e5a773d3458c7
ffmmx0001-creator <ffmmx0001@gmail.com>
2026-03-31 23:36:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -315,7 +315,7 @@ def add_handlers(app):
 
 if __name__ == "__main__":
     load_data()
-    app = ApplicationBuilder().token(8690035524:AAGrORyu8MKUQw1uZwM3h8Om_z5kmWEdZdM).build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
     add_handlers(app)
     print("✅ Sinzhu Bot Starting...")
     app.run_polling(drop_pending_updates=True)
